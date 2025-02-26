296d0f1eb480c791ef759b38ba2858ad257ea4b5
shubham <shubhshri25gmail.com>
2025-02-26 14:08:14 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,22 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+
+// Serve static assets if in production (only works in production)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   connectDb();
